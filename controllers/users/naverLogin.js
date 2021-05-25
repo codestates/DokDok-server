c8afff92c99000335b0e7e5b5a876ff78601918c
chotaegyu <chotg5592@gmail.com>
2021-05-25 10:39:32 +0900

    feat:소셜로그인

--- controllers/users/naverLogin.js
+++ controllers/users/naverLogin.js
@@ -7,7 +7,7 @@ const naverLogin = (req, res) => {
     로그인 버튼
     https://nid.naver.com/oauth2.0/authorize?response_type=code&client_id=Geic3o3F4WAETCidA5Yx&state=STATE_STRING&redirect_uri=http://localhost:4000/users/naver/callback
    */
-  res.redirect(
+  return res.redirect(
     `https://nid.naver.com/oauth2.0/authorize?response_type=code&client_id=${process.env.NAVER_CLIENT_ID}&state=STATE_STRING&redirect_uri=${process.env.NAVER_REDIRECT_URI}`,
   );
 };
@@ -61,11 +61,13 @@ const naverCallback = async (req, res) => {
       secure: false, // https
     });
 
-    res.status(200).send({
-      accessToken: accessToken,
-      user: payload,
-      message: 'login success',
-    });
+    res.redirect(`http://localhost:3000/main?access_token=${accessToken}`);
+
+    // res.status(200).send({
+    //   accessToken: accessToken,
+    //   user: payload,
+    //   message: 'login success',
+    // });
   } catch (error) {
     console.error(error);
     res.status(500).send({ message: '서버에러' });
